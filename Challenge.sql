d990a07e4bf947b612893c4fe61bb7dc044d177c
103620582 <103620582@student.swin.edu.au>
2021-06-29 11:24:00 +1000

    Task 2 Complete

--- Challenge.sql
+++ Challenge.sql
@@ -22,3 +22,67 @@
 
 -- TEACHER(StaffID, Surname, GivenName)
 -- PK - StaffID
+
+/*
+SELECT name, database_id, create_date 
+FROM sys.databases ;
+*/
+
+USE S103620582;
+
+
+DROP TABLE IF EXISTS ENROLEMENT;
+DROP TABLE IF EXISTS SUBJECTOFFERING;
+DROP TABLE IF EXISTS STUDENT;
+DROP TABLE IF EXISTS SUBJECT;
+DROP TABLE IF EXISTS TEACHER;
+
+GO
+
+CREATE TABLE SUBJECT(
+    SubjCode NVARCHAR(100)
+,   Description NVARCHAR(500)
+,   PRIMARY KEY (SubjCode)
+);
+
+CREATE TABLE STUDENT(
+    StudentID NVARCHAR(10)
+,   Surname NVARCHAR(100) NOT NULL
+,   GivenName NVARCHAR(100) NOT NULL
+,   Gender NVARCHAR(1) CHECK (Gender IN ('M', 'F', 'I'))
+,   PRIMARY KEY (StudentID)
+);
+
+CREATE TABLE TEACHER(
+    StaffID INT CHECK (StaffID IN (Len(8)))
+,   Surname NVARCHAR(100) NOT NULL
+,   GivenName NVARCHAR(100) NOT NULL
+,   PRIMARY KEY (StaffID)
+);
+
+CREATE TABLE SUBJECTOFFERING(
+    SubjCode NVARCHAR (100)
+,   Year INT CHECK (Year IN (Len(4)))
+,   Semester INT CHECK (Semester IN (1, 2))
+,   Fee MONEY NOT NULL CHECK (Fee > 0)
+,   StaffID INT CHECK (StaffID IN (Len(8)))
+,   PRIMARY KEY (SubjCode, Year, Semester)
+,   FOREIGN KEY (SubjCode) REFERENCES SUBJECT (SubjCode)
+,   FOREIGN KEY (StaffID) REFERENCES TEACHER (StaffID)
+);
+
+CREATE TABLE ENROLEMENT(
+    StudentID NVARCHAR(10)
+,   SubjCode NVARCHAR(100)
+,   Year INT CHECK (Year IN (Len(4)))
+,   Semester INT CHECK (Semester IN (1, 2))
+,   Grade NVARCHAR(2) CHECK (Grade IN ('N', 'P,', 'C', 'D', 'HD')) DEFAULT NULL
+,   DateEnrolled DATE
+,   PRIMARY KEY (StudentID, SubjCode, Year, Semester)
+,   FOREIGN KEY (StudentID) REFERENCES STUDENT (StudentID)
+,   FOREIGN KEY (SubjCode, Year, Semester) REFERENCES SUBJECTOFFERING (SubjCode, Year, Semester)
+);
+
+
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES
